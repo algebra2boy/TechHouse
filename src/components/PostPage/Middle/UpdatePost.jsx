81596e5bb4d4478ad62dad501febda713145ca98
Yongye Tan <yongyetan1209@gmail.com>
2023-11-05 17:17:10 -0500

    Finish deleting a task

--- src/components/PostPage/Middle/UpdatePost.jsx
+++ src/components/PostPage/Middle/UpdatePost.jsx
@@ -1,18 +1,35 @@
 import pen from "/pen.jpeg";
 import trashcan from "/trashcan.png";
 
-const UpdateVote = () => {
+import supabase from "../../../Database/supabase.js";
+
+const UpdateVote = ({ post }) => {
+
+    const deleteHandler = async (postID) => {
+
+        await supabase
+            .from("Posts")
+            .delete()
+            .eq("id", postID);
+
+        window.location = "/";
+    }
+
     return (
         <div className="middle-right-side">
             <Icon role={"edit"} url={pen} />
-            <Icon role={"delete"} url={trashcan} />
+            <Icon role={"delete"} url={trashcan} action={() => deleteHandler(post.id)} />
         </div>
     )
 };
 
-const Icon = ({ role, url }) => {
+const Icon = ({ role, url, action }) => {
     return (
-        <img src={url} alt={role} className="icon-image ${role}" />
+        <img
+            src={url}
+            alt={role}
+            className="icon-image ${role}"
+            onClick={action} />
     )
 }
 
