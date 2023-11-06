204c23fa5f20ef13b2b45fb2b80bfad3dc8854c7
Yongye Tan <yongyetan1209@gmail.com>
2023-11-06 18:11:48 -0500

    Hide my url and api key and ignore env key/value

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = "https://hjlgbhubjtquybvhrind.supabase.co";
-const APIKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbGdiaHVianRxdXlidmhyaW5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwNjU4MDYsImV4cCI6MjAxNDY0MTgwNn0.LKZKYUq1oxyw8V4RVOP09_UkfPofGOn1fMCtypJlZ8A";
+const url = import.meta.env.VITE_APP_SUPABASE_URL;
+const APIKEY = import.meta.env.VITE_APP_SUPABASE_APIKEY;
 
 // establish a client connection to superbase
 const database = createClient(url, APIKEY);
